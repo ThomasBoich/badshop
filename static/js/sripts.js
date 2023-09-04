2c80aed47349775d05d477311108108a877735cb
ARTEM BOYKO <boyko.a.v@bk.ru>
2023-09-04 16:17:47 +0700

    АДАПТИВ ГЛАВНАЯ

---
+++ static/js/sripts.js
@@ -0,0 +1,24 @@
+window.addEventListener('DOMContentLoaded', function() {
+    function setViewportScale() {
+      const screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  
+      let viewportTag = document.querySelector('meta[name="viewport"]');
+      let viewportContent = 'width=device-width, initial-scale=1.0';
+  
+      if (screenWidth < 370) {
+        viewportContent = 'width=device-width, initial-scale=0.8'; // Измените значение 1.2 на желаемый масштаб
+      }
+  
+      if (!viewportTag) {
+        viewportTag = document.createElement('meta');
+        viewportTag.setAttribute('name', 'viewport');
+        document.head.appendChild(viewportTag);
+      }
+  
+      viewportTag.setAttribute('content', viewportContent);
+    }
+  
+    setViewportScale();
+  
+    window.addEventListener('resize', setViewportScale);
+  });
